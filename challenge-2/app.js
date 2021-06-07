c0273794e0a8f278a817f424d3bbdc2524dc3813
Dandun Adi <dandun.adi@gmail.com>
2021-06-07 23:28:20 +0700

    add modal functionality

--- challenge-2/app.js
+++ challenge-2/app.js
@@ -1,5 +1,9 @@
 const pricingCards = document.querySelectorAll(".pricing .card");
-// console.log(pricingCards);
+const buyButtons = document.querySelectorAll(".pricing-btn");
+const modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".modal-close");
+
+// Pricing card active state when clicked
 pricingCards.forEach((card) => {
   card.addEventListener("click", () => {
     deselectAllPricingCards();
@@ -12,3 +16,15 @@ const deselectAllPricingCards = () => {
     card.classList.remove("active");
   });
 };
+
+// Show modal popup on pricing button
+buyButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+  });
+});
+
+// Close modal when button clicked
+modalCloseBtn.addEventListener("click", () => {
+  modal.classList.toggle("hidden");
+});
